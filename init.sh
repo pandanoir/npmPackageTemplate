331c73477714210e2c75436d2fe3a13c0c43d3b6
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2017-10-27 23:37:54 +0900

    removed unnecessary backslash

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!bin/sh
+rm -rf .git
 if type npm >/dev/null 2>&1; then
     npm init --yes
     npm install --save-dev mocha rollup
@@ -16,7 +17,7 @@ name=$(basename `pwd`)
 moduleName=$(echo|awk -v "var=$name" "{print toupper(substr(var,1,1))substr(var,2);}")
 fileName=$(echo|awk -v "var=$name" "{print tolower(substr(var,1,1))substr(var,2);}")
 
-sed -e 's/\"test\": \".*\"/\"preversion\": \"npm test\",\n    \"version\": \"npm run build \&\& gulp \&\& git add -A\",\n    \"postversion\": \"git push \&\& git push --tags\",\n    \"pretest\": \"npm run build\",\n    \"test\": \"mocha -R spec\",\n    \"build\": \"rollup src\/main.js -c -o dist\/'$fileName'.js --name '$moduleName'\"/g' -e 's/\"main\": \".*\"/\"main\": \"dist\/'$fileName'.js\"/g' package.json > package.tmp.json
+sed -e 's/"test": ".*"/"preversion": "npm test",\n    "version": "npm run build \&\& gulp \&\& git add -A",\n    "postversion": "git push \&\& git push --tags",\n    "pretest": "npm run build",\n    "test": "mocha -R spec",\n    "build": "rollup src\/main.js -c -o dist\/'$fileName'.js --name '$moduleName'"/g' -e 's/"main": ".*"/"main": "dist\/'$fileName'.js"/g' package.json > package.tmp.json
 cp package.tmp.json package.json
 rm package.tmp.json
 
